66d48ad46174d8318791f4bf20d44b250f47fa5d
Javier Sánchez <javiershoz@gmail.com>
2018-01-22 20:42:26 +0100

    Fin de clase

---
+++ Assets/Scripts/GazeSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GazeSystem : MonoBehaviour {
+
+
+	private Collider actualCollider;
+	private float startTime;
+
+	[SerializeField] //PARA QUE SALGA EN INTERFAZ DE UNITY
+	private float loadTime;
+
+	[SerializeField]
+	private Image loader;
+
+	void OnTriggerEnter(Collider colisionado) { // YO (VARA) HE CHOCADO CON "COLISIONADO"!
+		if(actualCollider == null) { 		//SI ANTES ESTABA CHOCANDO CON "NADA"
+			actualCollider = colisionado; //AHORA ESTOY COLISIONANDO CON "COLISIONADO"
+			startTime = Time.time; //EMPEZAMOS A CONTAR EN TIME.TIME (SEGUNDOS DESDE PLAY)
+		}
+	}
+
+	void OnTriggerStay(Collider colisionado) { //SEGUIMOS CHOCANDO CON ALGO
+		if (actualCollider == colisionado) { //ESE ALGO ES COLISIONADO DE ONTRIGGERENTER?
+			float tiempoQueHaPasado = (Time.time - startTime); 
+			float porcentajeRellenado = tiempoQueHaPasado / loadTime;
+			loader.fillAmount = porcentajeRellenado; //RELLENAMOS EL CIRCULITO DEL LOADER
+		}
+	}
+
+	void OnTriggerExit(Collider colisionado) {
+		actualCollider = null;
+		loader.fillAmount = 0;
+	}
+		
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
